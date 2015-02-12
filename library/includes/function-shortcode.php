d153d5a2bcbcba0e08d5217eecd6770d2fe8c2b8
luetkemj <luetkemj@gmail.com>
2015-02-12 12:30:41 -0500

    remove some old boilerplate

--- library/includes/function-shortcode.php
+++ library/includes/function-shortcode.php
@@ -16,12 +16,3 @@ function lmj_slideshow() {
 add_shortcode( 'slideshow', 'lmj_slideshow' );
 
 
-// [bartag foo="foo-value"]
-function ica_level( $atts, $content = null ) {
-	extract( shortcode_atts( array(
-		
-	), $atts ) );
-
-	return "<div class='gradient'>{$content}</div>";
-}
-add_shortcode( 'gradient', 'ica_level' );
